c8ea53c69633ef4222fd2943d587e91f0e66bda7
jamesliudotcc <james@jamesliu.cc>
2018-12-04 11:50:31 -0800

    working with clicking item to strike item through

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
-console.log('loaded');
-
 $('ul').on('click', 'li', function() {
-  this.remove();
+  $(this).css('text-decoration', 'line-through');
 });
 
 $('#addToDo').click(function() {
-  console.log('button is clicked', $('#input').val());
   $('ul')
     .first()
     .append(`<li>${$('#input').val()}</li>`);
